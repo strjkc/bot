7f1b3b518bd1fc1b5893e3edb7be8632d1eda575
strahinja <strahinjaa.va@gmail.com>
2020-12-02 22:44:26 +0100

    heroku procfile

--- bot.js
+++ bot.js
@@ -2,77 +2,54 @@ const Discord = require('discord.js');
 const config = require('./config.js')
 const client = new Discord.Client();
 const timestamps = [
-    '9:50:00 AM',
-    '10:00:00 AM',
-    '10:50:00 AM',
-    '11:00:00 AM',
-    '11:50:00 AM',
-    '12:00:00 AM',
-    '12:50:00 AM',
-    '01:30:00 PM',
-    '02:20:00 PM',
-    '02:30:00 PM',
-    '03:20:00 PM',
-    '02:30:00 PM',
-    '04:20:00 PM',
-    '05:00:00 PM',
-]
-const testStamps = [
     [
-        '7:50',
-        '7:52',
-        '7:54',
-        '7:56',
-        '7:58',
-        '7:60',
-        '7:62',
+        '9:50',
+        '10:50',
+        '11:50',
+        '12:50',
+        '2:20',
+        '3:20',
+        '4:20',
 
     ],
     [
-        '7:51',
-        '7:53',
-        '7:55',
-        '7:57',
-        '7:59',
-        '7:61',
-        '7:63',
+        '10:00',
+        '11:00',
+        '12:00',
+        '1:30',
+        '2:30',
+        '3:30',
+        '4:30'
     ]
 ]
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-let Message = '!startat 9:10'
-let messageToSend = 'Default message'
 client.on('message', message => {
     console.log(message.content)
     if (message.content.includes('!startat')){
-        console.log('starting outer fun')
         const r = message.content.substring(9)
         const id = setInterval(() =>{
             let time = new Date().toLocaleTimeString().substring(0,4)
-            console.log(r,)
             if (time === r){
                 clearInterval(id)
-                console.log('interval cleared')
                 setInterval( () => {
-                    console.log('inner starting')
                     let time = new Date().toLocaleTimeString()
                     console.log(time)
-                    console.log(testStamps[0].includes(time))
+                    console.log(timestamps[0].includes(time))
                     if (time.substring(0,4) === '9:00' && time.includes('AM'))
                         message.send('Dobro jutro :)')
                     else if (time.substring(0,4) === '5:00' && time.includes('PM'))
                         message.send('Dobro jutro :)')
-                    else if (testStamps[0].includes(time.substring(0,4)))
+                    else if (timestamps[0].includes(time.substring(0,4)))
                         message.channel.send('PAUYA <3 @everyone')
-                    else if (testStamps[1].includes(time.substring(0,4)))
+                    else if (timestamps[1].includes(time.substring(0,4)))
                         message.channel.send('GOTOVA PAUYA :( @everyone')
                 }, 60000)
             }
-        },1000)
-        console.log('running')
-        
+        },1000)        
     }
 
 })
